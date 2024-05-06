6685fa4e60802d469e7a507c5884fbc6a97b311b
gohil rohit <gohilrohit309@gmail.com>
2024-05-06 22:31:11 +0530

    add flies

--- api-crudopration/src/App.js
+++ api-crudopration/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 }
--- api-crudopration/src/Components/Home.jsx
+++ api-crudopration/src/Components/Home.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
 
 
 
 function Home(){
+
+    let[data,setData] =  useState({});
+
+    let getValue = (e)=>{
+        let name = e.target.name;
+        let value = e.target.value;
+        setData({...data,[name]:value});
+    }
+    let submitData =(e)=>{
+        e.preventDefault()
+        fetch("http://localhost:3000/data",{
+            method:"POST",
+            body : JSON.stringify(data)
+        }).then(()=>{
+            alert("data added") 
+            setData({})
+        })
+        .catch(()=>{
+            alert("something wrong")
+        })
+    }
+
     return(
         <>
-            <h1>helllo</h1>
+            <form method="post" onSubmit={(e)=>submitData(e)}>
+
+                <label>User name :- </label>
+                <input type="text" name="userName" value={data.userName?data.userName :""} placeholder="Enter Your User Name" onChange={(e)=>getValue(e)} /><br/><br/>
+                <label>User email :- </label>
+                <input type="text" name="mail" value={data.mail?data.mail :""} placeholder="Enter Your User Email" onChange={(e)=>getValue(e)} /><br/><br/>
+                <label>Password :- </label>
+                <input type="text" name="pass" value={data.pass?data.pass:""} placeholder="Enter Your Password" onChange={(e)=>getValue(e)} /><br/><br/>
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 }
